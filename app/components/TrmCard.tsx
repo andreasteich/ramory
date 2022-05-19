6aa4fc1a027ea10fc3a6e70982a8fb3dabea9dc3
Andreas Teich <andreas.teich@icloud.com>
2022-05-19 16:55:51 +0200

    Add animations

--- app/components/TrmCard.tsx
+++ app/components/TrmCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { motion } from 'framer-motion'
 
 type Props = {
     id: string;
@@ -9,11 +9,23 @@ type Props = {
 
 export default function TrmCard({ id, clicked, imageUrl, cardClicked }: Props) {
     return (
-        <div 
+        <motion.div 
             className="shadow-lg hover:cursor-pointer rounded-2xl h-40 bg-pink-500 flex flex-col justify-center"
             onClick={() => cardClicked(id)}
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 0.3 }}
+            variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0 }
+            }}
+            whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.1 },
+            }}
+            whileTap={{ rotate: 360 }}
         >
             { clicked ? <img src={'/' + imageUrl} className="object-cover rounded-2xl" /> : <p className="text-white my-0 mx-auto font-bold text-2xl">TRM</p> }
-        </div>
+        </motion.div>
     )
 }
