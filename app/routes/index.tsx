038751cf78fefdd5ed8fdfdd8caa348bb6dc5cc6
Andreas Teich <andreas.teich@icloud.com>
2022-05-29 18:56:17 +0200

    Add template with sections

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ import { ArrowRightIcon } from "@heroicons/react/outline";
 import { ActionFunction, redirect } from "@remix-run/cloudflare";
 import { Form } from "@remix-run/react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useToast } from "~/contexts/ToastContext";
 import { constructUrlForDo } from "~/utils";
 
@@ -30,30 +31,60 @@ export default function Index() {
   const { addToast } = useToast();
 
   return (
-    <div className="flex flex-col gap-20 items-center mx-auto my-0 h-full justify-center">
-      <div className="flex flex-col gap-10">
-        <h1 className="text-center font-bold text-6xl" onClick={() => addToast("hallo")}>RAMory</h1>
-        <h2 className="text-center font-semibold text-4xl">How good is your card's memory?</h2>
+    <div className="flex flex-col">
+      <div className="flex flex-col gap-16 items-start border-y border-black px-8 py-16">
+        <p className="text-4xl">RAMory 🎸</p>
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-6xl font-extrabold">How good is your memory?</h1>
+            <h2 className="text-lg">Create a board and send it to friends to compete with their memory.</h2>
+          </div>
+          <Form method="post" className="w-full">
+            <button 
+              type="submit" 
+              className="shadow-[2px_2px_0_0_rgba(0,0,0,1)] bg-[#69E290] w-full rounded-lg border-2 border-black py-2 font-bold"
+            >Let's RAMble</button>
+          </Form>
+        </div>
       </div>
-      <Form method="post" className="flex flex-row items-center gap-5 justify-evenly w-full">
-        <div className="flex flex-col gap-2">
-          <p className="font-semibold text-sm">How many Players?</p>
-          <input type="number" min="1" max="10" placeholder="1-10" className="" name="allowedPlayersInTotal" required />
-        </div> 
-        <div className="flex flex-col gap-2">
-          <p className="font-semibold text-sm">Topic</p>
-          <div className="flex flex-row gap-1 items-center">
-            <input type="radio" id="fruits" name="topic" value="fruits" defaultChecked />
-            <label htmlFor="fruits">Fruits</label>
+      <div className="flex flex-col gap-16 items-start bg-[#F5CAD1] border-y border-black px-8 py-16">
+        <div className="flex flex-col gap-8 items-start">
+          <img src="take-a-slot.png" className="object-contain" />
+          <div className="flex flex-col gap-4">
+            <h3 className="text-lg font-bold">📥 Take a slot</h3>
+            <p>Create or join a board and put your card into a available slot.</p>
           </div>
         </div>
-        <div>
-          <button type="submit" className="flex flex-row gap-2 items-center py-2 px-4 text-center bg-pink-500 hover:bg-pink-600 text-white rounded-lg">
-            Let's find out
-            <ArrowRightIcon className="h-4" />
-          </button>
+      </div>
+      <div className="flex flex-col gap-16 items-start bg-[#ED6FA0] border-y border-black px-8 py-16">
+        <div className="flex flex-col gap-8 items-start">
+          <img src="find-matching-silicons.png" className="object-contain" />
+          <div className="flex flex-col gap-4">
+            <h3 className="text-lg font-bold">💠️ Find matching silicons</h3>
+            <p>Try to find similiar chips to get RAM. Every matching pair increments your RAM by 1000 mb, but avoid different one. They will make you loose 100mb.</p>
+          </div>
         </div>
-      </Form>
+      </div>
+      <div className="flex flex-col gap-16 items-start bg-[#82EDE0] border-y border-black py-16">
+        <div className="flex flex-col gap-8 items-start">
+          <img src="collect-ram.png" className="object-contain" />
+          <div className="flex flex-col px-8 gap-4">
+            <h3 className="text-lg font-bold">🤑 Collect RAM</h3>
+            <p>Increase your total RAM by finding matching silicons. At the end the card with the most memory wins.</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-16 items-start border-y border-black px-8 py-16">
+        <Form method="post" className="w-full">
+          <button 
+            type="submit" 
+            className="bg-black text-white w-full rounded-lg py-2 font-bold"
+          >Create a board</button>
+        </Form>
+      </div>
+      <div className="flex bg-gray-200 flex-col gap-16 items-start border-y border-black px-8 py-16">
+        <p>Footer</p>
+      </div>
     </div>
   );
 }
