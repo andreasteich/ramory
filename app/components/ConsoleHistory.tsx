a28f06a34b477af9575188cb597adda4d701da62
Andreas Teich <andreas.teich@icloud.com>
2022-05-31 12:00:07 +0200

    Align ui

--- app/components/ConsoleHistory.tsx
+++ app/components/ConsoleHistory.tsx
@@ -1,11 +1,11 @@
 type Props = {
-    notifications: any[]
+    boardHistory: any[]
 }
 
-export default function ConsoleHistory({ notifications }: Props) {
+export default function ConsoleHistory({ boardHistory }: Props) {
     return (
         <div className="rounded-t-lg font-mono bg-black border-x-2 border-t-2 border-gray-600">
-        { notifications.map(({ type, from, message }, index) => (
+        { boardHistory.map(({ type, from, message }, index) => (
             <p key={index} className="text-white px-2 py-1">{from}: <strong>{message}</strong></p>
         ))}
         </div>
--- app/components/ConsoleInput.tsx
+++ app/components/ConsoleInput.tsx
@@ -1,11 +1,11 @@
 type Props = {
-    notification: any | undefined
+    currentHistorySlice: any | undefined
 }
 
-export default function ConsoleInput({ notification }: Props) {
+export default function ConsoleInput({ currentHistorySlice }: Props) {
     return (
         <div className="rounded-b-lg font-mono border-x-2 border-b-2 border-gray-600 bg-black">
-            <p className="text-white px-2 py-1">{notification?.from}: <strong>{notification?.message}</strong></p>
+            <p className="text-white px-2 py-1">{currentHistorySlice?.from}: <strong>{currentHistorySlice?.message}</strong></p>
         </div>
     )
 }
