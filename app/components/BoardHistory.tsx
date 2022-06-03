4019c5ebbfc83727f782d7c082dd26fd7007bb4e
Andreas Teich <andreas.teich@icloud.com>
2022-06-03 23:44:58 +0200

    fix win and loose slices

--- app/components/BoardHistory.tsx
+++ app/components/BoardHistory.tsx
@@ -8,7 +8,12 @@ export default function BoardHistory({ boardHistory }: Props) {
     return (
         <div className="flex flex-col justify-end gap-1 rounded-lg font-mono text-sm bg-black border-2 border-gray-600 overflow-y-auto h-[10vh] lg:h-[75vh]">
         { boardHistory.map(({ type, from, message }, index) => (
-            <p key={index} className={`${historySliceTextColor(type)} px-2 py-1`}>{from}: <strong>{message}</strong></p>
+            <p key={index} className={`${historySliceTextColor(type)} px-2 py-1`}>
+                {from}: <strong>{message}</strong>
+                { type === 'end' && (
+                <p>Created with 🤙🏻 by <a className="text-blue-600" href="https://twitter.com/Andifined97" target="_blank">@Andifined97</a> and <a className="text-blue-600" href="https://twitter.com/MatthiasSiml" target="_blank">@dev-hias</a></p>
+                )}
+            </p>
         ))}
         </div>
     )
